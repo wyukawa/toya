0c3537638d67832a81dcf1c9bde6c4f5aee19418
wyukawa <wyukawa@gmail.com>
2016-04-26 14:07:48 +0900

    Add web application logic

--- toya.go
+++ toya.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"net/http"
 
 	"github.com/prometheus/common/log"
 	"github.com/prometheus/prometheus/config"
@@ -10,7 +12,8 @@ import (
 )
 
 var (
-	configFile = flag.String("config.file", "prometheus.yml", "prometheus configuration file name.")
+	configFile    = flag.String("config.file", "prometheus.yml", "prometheus configuration file name.")
+	listenAddress = flag.String("listen-address", ":12345", "The address to listen on for HTTP requests.")
 )
 
 func main() {
@@ -22,25 +25,32 @@ func main() {
 	if err != nil {
 		log.Errorf("Couldn't load configuration (-config.file=%s): %v", *configFile, err)
 	} else {
-		newcfg := &config.ScrapeConfig{}
-		newcfg.JobName = "aaa"
-		fsdc := &config.FileSDConfig{}
-		fsdc.Names = []string{"aaa.yaml"}
-		newcfg.FileSDConfigs = append(newcfg.FileSDConfigs, fsdc)
-		rc := &config.RelabelConfig{}
-		rc.Regex = config.MustNewRegexp("(.*):\\d+")
-		newcfg.RelabelConfigs = append(newcfg.RelabelConfigs, rc)
+		log.Infof("Starting Server: %s", *listenAddress)
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			newcfg := &config.ScrapeConfig{}
+			newcfg.JobName = "aaa"
+			fsdc := &config.FileSDConfig{}
+			fsdc.Names = []string{"aaa.yaml"}
+			newcfg.FileSDConfigs = append(newcfg.FileSDConfigs, fsdc)
+			rc := &config.RelabelConfig{}
+			rc.Regex = config.MustNewRegexp("(.*):\\d+")
+			newcfg.RelabelConfigs = append(newcfg.RelabelConfigs, rc)
 
-		var newScrapeConfigs []*config.ScrapeConfig
-		newScrapeConfigs = append(newScrapeConfigs, newcfg)
-		for _, scfg := range conf.ScrapeConfigs {
-			newScrapeConfigs = append(newScrapeConfigs, scfg)
-		}
-		newYamlData, err := yaml.Marshal(&newScrapeConfigs)
+			var newScrapeConfigs []*config.ScrapeConfig
+			newScrapeConfigs = append(newScrapeConfigs, newcfg)
+			for _, scfg := range conf.ScrapeConfigs {
+				newScrapeConfigs = append(newScrapeConfigs, scfg)
+			}
+			newYamlData, err := yaml.Marshal(&newScrapeConfigs)
+			if err != nil {
+				log.Fatalf("error: %v", err)
+			}
+			fmt.Fprint(w, string(newYamlData))
+		})
+		err := http.ListenAndServe(*listenAddress, nil)
 		if err != nil {
 			log.Fatalf("error: %v", err)
 		}
-		log.Infof(string(newYamlData))
 	}
 
 }
