bae15c178fe9dca0b691cfa293c59dcc2d44610e
wyukawa <wyukawa@gmail.com>
2016-04-27 16:34:04 +0900

    Marshal new data

--- toya.go
+++ toya.go
@@ -3,10 +3,13 @@ package main
 import (
 	"flag"
 	"html/template"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/prometheus/common/log"
 	"github.com/prometheus/prometheus/config"
+
+	"gopkg.in/yaml.v2"
 )
 
 var (
@@ -25,10 +28,37 @@ func main() {
 	} else {
 		log.Infof("Starting Server: %s", *listenAddress)
 		t := template.Must(template.ParseFiles("templates/toya.html"))
-		http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static")))) 
+		http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			defer r.Body.Close()
 			if r.Method == "GET" {
 				t.Execute(w, conf.ScrapeConfigs)
+			} else if r.Method == "POST" {
+				//exporter_list = r.FormValue("exporter_list")
+				newcfg := &config.ScrapeConfig{}
+				newcfg.JobName = r.FormValue("job_name")
+				fsdc := &config.FileSDConfig{}
+				fsdc.Names = []string{"aaa.yaml"}
+				newcfg.FileSDConfigs = append(newcfg.FileSDConfigs, fsdc)
+				rc := &config.RelabelConfig{}
+				rc.Regex = config.MustNewRegexp("(.*):\\d+")
+				newcfg.RelabelConfigs = append(newcfg.RelabelConfigs, rc)
+
+				var newScrapeConfigs []*config.ScrapeConfig
+				newScrapeConfigs = append(newScrapeConfigs, newcfg)
+				for _, scfg := range conf.ScrapeConfigs {
+					newScrapeConfigs = append(newScrapeConfigs, scfg)
+				}
+				conf.ScrapeConfigs = newScrapeConfigs
+				newYamlData, err := yaml.Marshal(&conf)
+				if err != nil {
+					log.Fatalf("error: %v", err)
+				}
+				writeErr := ioutil.WriteFile(*configFile, newYamlData, 0644)
+				if writeErr != nil {
+					log.Fatalf("error: %v", writeErr)
+				}
+				t.Execute(w, newScrapeConfigs)
 			}
 		})
 		err := http.ListenAndServe(*listenAddress, nil)
